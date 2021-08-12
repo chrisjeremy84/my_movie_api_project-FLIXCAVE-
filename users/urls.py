ac306aaa68453a8af7f6f6910a48ef01f42c4a3e
5thfader <chrisjeremy84@live.com>
2021-08-12 10:01:09 +0200

    Profile system Implemented, Styling required

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
    path('', views.search , name='users-search'),
     path('login/', views.login , name='login'),
+    path('profile/', views.profile, name='profile') ,
     path('logout/', views.login , name='logout'),
     path('help/', views.help , name='help'),
     path('register/', views.register, name='register'),
     path('login/', views.help , name='help')
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from .form import UserRegisterForm
+from .form import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+
 
 # Create your views here.
 def search(request):
@@ -29,3 +31,27 @@ def register(request):
 
 def help(request):
     return render( request, 'help/help.html')
+
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, 
+                                    request.FILES, 
+                                    instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+            'u_form' : u_form,
+            'p_form' : p_form
+            }
+
+    return render(request, 'profile/profile.html', context)
